593357ed2bb48f846359a2ceaf7c715cdf64d6ba
Adarsh Pandey <meadarshpandey@gmail.com>
2023-08-17 17:17:17 +0530

    Second Version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* ============================typing animation ============================ */
 var typed = new Typed(".typing", {
-    strings: ["Web Designer", "Web Developer", "Tech Innovator", "Content Creator"],
+    strings: ["Web Designer", "Web Developer", "YouTuber"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
