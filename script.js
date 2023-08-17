bef17df336944d70db392c773e2cb281ac9be126
Adarsh Pandey <meadarshpandey@gmail.com>
2023-08-17 14:53:33 +0530

    First Version

---
+++ script.js
@@ -0,0 +1,43 @@
+/* ============================typing animation ============================ */
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Tech Innovator", "Content Creator"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+
+/* ============================ Aside ============================ */
+const nav = document.querySelector(".nav"),
+navList = nav.querySelectorAll("li"),
+totalNavList = navList.length;
+for(let i=0; i<totalNavList; i++)
+{
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function()
+    {
+        for(let j=0; j<totalNavList; j++)
+        {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+}
+
+
+/* ============================ Mobile Menu ============================ */
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () =>
+{
+    asideSectionTogglerBtn();
+})
+function asideSectionTogglerBtn()
+{
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for(let i=0; i<totalSection; i++)
+    {
+        allSection[i].classList.toggle("open");
+    }
+}
